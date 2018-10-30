754cf47c0b69799dba215a32a157b61675fa164c
topnuomi <1130395124@qq.com>
2018-10-30 19:28:27 +0800

    更新自定义路由

--- Application/route.php
+++ Application/route.php
@@ -26,39 +26,15 @@ return [
         '[keywords][:category]',
         'Home/Article/search'
     ],
-	'manage' => [
-		null,
-		'Manage'
-	],
+    'manage' => [
+        null,
+        'Manage'
+    ],
     'login' => [
         null,
         'Manage/Auth/index'
     ],
 
     // 其他新增路由
-
-    'synopsis' => [
-        null,
-        'Home/Article/detail/category/synopsis/id/1'
-    ],
-    'contact' => [
-        null,
-        'Home/Article/detail/category/contact/id/2'
-    ],
-    'environment' => [
-        null,
-        'Home/Article/lists/category/environment'
-    ],
-    'news' => [
-        null,
-        'Home/Article/lists/category/news'
-    ],
-    'search' => [
-        '[keywords]',
-        'Home/Article/search'
-    ],
-    'words' => [
-        null,
-        'Home/Words/index'
-    ]
+    
 ];
